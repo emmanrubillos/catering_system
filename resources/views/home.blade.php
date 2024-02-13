92650352d169c62aef9913ac990124b873e1bdec
emmanrubillos <126300648+emmanrubillos@users.noreply.github.com>
2024-02-13 15:43:52 +0800

    fixed the dubplicate dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,3 @@
-@extends('layouts.app')
 @extends('layouts.master')
 @extends('sidebar.dashboard')
 @section('content')
@@ -9,21 +8,7 @@
                 <div class="row">
                     <div class="col-sm-12 mt-5">
                         <div class="row justify-content-center">
-                            <div class="col-md-8">
-                                <div class="card">
-                                    <div class="card-header">{{ __('Dashboard') }}</div>
-                    
-                                    <div class="card-body">
-                                        @if (session('status'))
-                                            <div class="alert alert-success" role="alert">
-                                                {{ session('status') }}
-                                            </div>
-                                        @endif
-                    
-                                        {{ __('You are logged in!') }}
-                                    </div>
-                                </div>
-                            </div>
+                            
                         </div>
                         <h3 class="page-title mt-3">Good Morning </h3>
                         <ul class="breadcrumb">
