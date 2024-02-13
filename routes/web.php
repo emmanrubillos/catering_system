27e8773524469db9fce87e3a0b8c047a5615e8ca
div-vince <dd.utrera@mlgcl.edu.ph>
2024-02-13 13:35:51 +0800

    first push

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,7 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/profile', [App\Http\Controllers\HomeController::class, 'profile'])->middleware('auth')->name('profile');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
