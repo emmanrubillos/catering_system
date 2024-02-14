a20f9db00dcb37d6cb76fa1d6537eca33b2fe5df
div-vince <dd.utrera@mlgcl.edu.ph>
2024-02-14 14:12:04 +0800

    Making Controller for Admin

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,23 @@
+@extends('admin.partials.navbar')
+@extends('admin.partials.sidebar')
+@section('content')
+<div class="container">
+    <div class="page-wrapper">
+        <div class="content container-fluid">
+            <div class="page-header">
+                <div class="row">
+                    <div class="col-sm-12 mt-5">
+                        <div class="row justify-content-center">
+                            
+                        </div>
+                        <h3 class="page-title mt-3"> Good Morning </h3>
+                        <ul class="breadcrumb">
+                            <li class="breadcrumb-item active">Your are in the Dashboard</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
