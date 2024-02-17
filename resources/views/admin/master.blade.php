a1ae1b08ad4fd20ff4da6c801a18ca601f42d3f4
div-vince <dd.utrera@mlgcl.edu.ph>
2024-02-17 12:05:01 +0800

    update the sidebar

--- resources/views/admin/master.blade.php
+++ resources/views/admin/master.blade.php
@@ -16,6 +16,9 @@
 	<link rel="stylesheet" type="text/css" href="{{ URL::to('assets/css/bootstrap-datetimepicker.min.css') }}">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 	<style>
+	a {
+		text-decoration: none;
+	}
 	.new-sidebar {
 		position: fixed;
 		top: 0;
@@ -94,7 +97,7 @@
 	
 	
 </body>
-<script data-cfasync="false" src="../../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
+	<script data-cfasync="false" src="../../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
 	<script src="{{ URL::to('assets/js/popper.min.js') }}"></script>
 	<script src="{{ URL::to('assets/js/bootstrap.min.js') }}"></script>
 	<script src="{{ URL::to('assets/plugins/slimscroll/jquery.slimscroll.min.js') }}"></script>
