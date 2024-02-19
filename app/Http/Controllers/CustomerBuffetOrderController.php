500665b1cec7f213cecb77d54f8afbbdd04390fa
mrdenzeru <ddaveutrer23@gmail.com>
2024-02-19 18:25:32 +0800

    Update SideBar Active Syntax

--- app/Http/Controllers/CustomerBuffetOrderController.php
+++ app/Http/Controllers/CustomerBuffetOrderController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class MenuController extends Controller
+class CustomerBuffetOrderController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -13,7 +13,7 @@ class MenuController extends Controller
      */
     public function index()
     {
-        return view ('admin.reservation.menu.index');
+        return view ('admin.reservation.buffet_order.index');
     }
 
     /**
