7ea96de3eedab51266273ab2cf0e1ee4ed774305
Joan Urgel <joanurgel55@gmail.com>
2024-02-19 16:13:46 +0800

    users frontend

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -4,14 +4,21 @@
     <div class="sidebar-inner">
         <div id="sidebar-menu" class="sidebar-menu">
             <ul>
-                <li class="active"> <a href="{{ route('admin') }}"><i class="fas fa-tachometer-alt"></i> <span>Dashboard</span></a> </li>
+                <li class="on-click"> <a href="{{ route('admin') }}"><i class="fas fa-tachometer-alt"></i> <span>Dashboard</span></a> </li>
                 <li class="list-divider"></li>
-                <li class="submenu"> <a href="#"><i class="fas fa-user"></i> <span> User </span> <span class="menu-arrow"></span></a>
-                    <ul class="submenu_class" style="display: none;">
-                        <li><a href="employees.html"> Admin </a></li>
-                        <li><a href="employees.html"> User </a></li>
+                <li class="submenu">
+                    <a href="#">
+                        <i class="fas fa-user"></i>
+                        <span> Users </span>
+                        <span class="menu-arrow"></span>
+                    </a>
+                    <ul class="submenu_class">
+                        <li><a href="{{ route('admin_user') }}" class="{{ request()->is('admin_user') ? 'active' : '' }}"> {!! request()->is('admin_user') ? '<b>Admin</b>' : 'Admin' !!} </a></li>
+                        <li><a href="{{ route('users') }}" class="{{ request()->is('users') ? 'active' : '' }}"> {!! request()->is('users') ? '<b>User</b>' : 'User' !!} </a></li>
                     </ul>
                 </li>
+                
+                
                 <li class="submenu"> <a href="#"><i class="fas fa-user"></i> <span> Reservation </span> <span class="menu-arrow"></span></a>
                     <ul class="submenu_class" style="display: none;">
                         <li><a href="#">Customer Reservation</a></li>
@@ -22,6 +29,7 @@
                 <li class="submenu"> 
                     <a href="#"><i class="fas fa-user"></i> <span> Menu </span></a>
                 </li>
+
                 <li class="submenu"> 
                     <a href="#"><i class="fas fa-user"></i> <span> Venue </span></a>
                 </li>
--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,9 @@ Route::get('/admin', [App\Http\Controllers\AdminController::class, 'index'])->na
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/profile', [App\Http\Controllers\HomeController::class, 'profile'])->middleware('auth')->name('profile');
 
+Route::get('/admin_user', [App\Http\Controllers\AdminUserController::class, 'index'])->name('admin_user');
+Route::get('/users', [App\Http\Controllers\UsersController::class, 'index'])->name('users');
+
 Route::middleware('CheckUserRole')->group(function (){
 
 });
