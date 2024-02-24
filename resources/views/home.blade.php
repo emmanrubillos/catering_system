4d48796b1eee76e296e3059310dc975dea46cc2f
Utrera, DD <126302609+mrdenzeru@users.noreply.github.com>
2024-02-24 20:03:15 +0800

    Tailwind CSS, Header, etc. (#36)
    * Tailwind Css & Header
    
    * Tailwind CSS, Header, etc
    
    ---------
    
    Co-authored-by: mrdenzeru <ddaveutrer23@gmail.com>

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,22 +1,6 @@
 @extends('layouts.master')
 @section('content')
-<div class="container">
-    <div class="page-wrapper">
-        <div class="content container-fluid">
-            <div class="page-header">
-                <div class="row">
-                    <div class="col-sm-12 mt-5">
-                        <div class="row justify-content-center">
-                            
-                        </div>
-                        <h3 class="page-title mt-3"> Good Morning </h3>
-                        <ul class="breadcrumb">
-                            <li class="breadcrumb-item active">This is Home</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+  <h1 class="text-3xl font-bold underline font-cursive">
+    This is Home Page
+  </h1>
 @endsection
