e373c675ff3353c63d6c6c479db423252947541c
mrdenzeru <ddaveutrer23@gmail.com>
2024-02-26 15:23:53 +0800

    minor updates

--- resources/views/layouts/inc/footer.blade.php
+++ resources/views/layouts/inc/footer.blade.php
@@ -1,3 +1,4 @@
+{{--! footer --}}
 <footer class="bg-dark w-full h-128 text-light">
     <div class="mx-auto px-10 pt-8 flex flex-row justify-between">
         <div class="column-one basis-1/3">
@@ -22,7 +23,7 @@
                             <lord-icon src="https://cdn.lordicon.com/iikoxwld.json" trigger="hover"
                                 colors="primary:#FF731D" style="width:30px;height:30px">
                             </lord-icon>
-                            <span class="text-sm font-semibold ml-2 mt-1 hover:text-primary">
+                            <span class="text-sm font-semibold mx-2 mt-1 hover:text-primary">
                                 Romano St. Poblacion, Hindang, Leyte
                             </span>
                         </a>
@@ -33,7 +34,7 @@
                             <lord-icon src="https://cdn.lordicon.com/xtzvywzp.json" trigger="hover"
                                 colors="primary:#FF731D" style="width:30px;height:30px">
                             </lord-icon>
-                            <span class="text-sm font-semibold ml-2 mt-1 hover:text-primary">
+                            <span class="text-sm font-semibold mx-2 mt-1 hover:text-primary">
                                 sonararosario@gmail.com
                             </span>
                         </a>
@@ -45,7 +46,7 @@
                                 <lord-icon src="https://cdn.lordicon.com/srsgifqc.json" trigger="hover"
                                     colors="primary:#FF731D" style="width:30px;height:30px">
                                 </lord-icon>
-                                <span class="text-sm font-semibold ml-2 mt-1 hover:text-primary">
+                                <span class="text-sm font-semibold mx-2 mt-1 hover:text-primary">
                                     0907-753-0219
                                 </span>
                             </a>
@@ -54,11 +55,7 @@
                 </ul>
             </div>
         </div>
-<<<<<<< HEAD
         <div class="column-two basis-1/3 flex justify-between">
-=======
-        <div class="column-two basis-1/3 laptop:basis-1/4 flex justify-between">
->>>>>>> 860bdbcd3aa94e659bed4edadb38155e0d2628d6
             {{-- C-two-Column-one --}}
             <div>
                 <h5 class="font-bold uppercase text-lg">
@@ -71,7 +68,7 @@
                             <lord-icon src="https://cdn.lordicon.com/xcrjfuzb.json" colors="primary:#FF731D"
                                 trigger="hover" style="width:30px;height:30px" class="-rotate-90">
                             </lord-icon>
-                            <span class="text-md ml-2 mt-1 hover:text-primary">
+                            <span class="text-md mx-2 mt-1 hover:text-primary">
                                 About Us
                             </span>
                         </a>
@@ -82,7 +79,7 @@
                             <lord-icon src="https://cdn.lordicon.com/xcrjfuzb.json" colors="primary:#FF731D"
                                 trigger="hover" style="width:30px;height:30px" class="-rotate-90">
                             </lord-icon>
-                            <span class="text-md ml-2 mt-1 hover:text-primary">
+                            <span class="text-md mx-2 mt-1 hover:text-primary">
                                 Services
                             </span>
                         </a>
@@ -93,7 +90,7 @@
                             <lord-icon src="https://cdn.lordicon.com/xcrjfuzb.json" colors="primary:#FF731D"
                                 trigger="hover" style="width:30px;height:30px" class="-rotate-90">
                             </lord-icon>
-                            <span class="text-md ml-2 mt-1 hover:text-primary">
+                            <span class="text-md mx-2 mt-1 hover:text-primary">
                                 Packages
                             </span>
                         </a>
@@ -104,7 +101,7 @@
                             <lord-icon src="https://cdn.lordicon.com/xcrjfuzb.json" colors="primary:#FF731D"
                                 trigger="hover" style="width:30px;height:30px" class="-rotate-90">
                             </lord-icon>
-                            <span class="text-md ml-2 mt-1 hover:text-primary">
+                            <span class="text-md mx-2 mt-1 hover:text-primary">
                                 Menu
                             </span>
                         </a>
@@ -115,7 +112,7 @@
                             <lord-icon src="https://cdn.lordicon.com/xcrjfuzb.json" colors="primary:#FF731D"
                                 trigger="hover" style="width:30px;height:30px" class="-rotate-90">
                             </lord-icon>
-                            <span class="text-md ml-2 mt-1 hover:text-primary">
+                            <span class="text-md mx-2 mt-1 hover:text-primary">
                                 Contact Us
                             </span>
                         </a>
@@ -126,7 +123,7 @@
                             <lord-icon src="https://cdn.lordicon.com/xcrjfuzb.json" colors="primary:#FF731D"
                                 trigger="hover" style="width:30px;height:30px" class="-rotate-90">
                             </lord-icon>
-                            <span class="text-md ml-2 mt-1 hover:text-primary">
+                            <span class="text-md mx-2 mt-1 hover:text-primary">
                                 Home
                             </span>
                         </a>
@@ -145,7 +142,7 @@
                             <lord-icon src="https://cdn.lordicon.com/xcrjfuzb.json" colors="primary:#FF731D"
                                 trigger="hover" style="width:30px;height:30px" class="-rotate-90">
                             </lord-icon>
-                            <span class="text-md ml-2 mt-1 hover:text-primary">
+                            <span class="text-md mx-2 mt-1 hover:text-primary">
                                 Privacy Policy
                             </span>
                         </a>
@@ -156,7 +153,7 @@
                             <lord-icon src="https://cdn.lordicon.com/xcrjfuzb.json" colors="primary:#FF731D"
                                 trigger="hover" style="width:30px;height:30px" class="-rotate-90">
                             </lord-icon>
-                            <span class="text-md ml-2 mt-1 hover:text-primary">
+                            <span class="text-md mx-2 mt-1 hover:text-primary">
                                 Terms and Conditions
                             </span>
                         </a>
@@ -167,7 +164,7 @@
                             <lord-icon src="https://cdn.lordicon.com/xcrjfuzb.json" colors="primary:#FF731D"
                                 trigger="hover" style="width:30px;height:30px" class="-rotate-90">
                             </lord-icon>
-                            <span class="text-md ml-2 mt-1 hover:text-primary">
+                            <span class="text-md mx-2 mt-1 hover:text-primary">
                                 Support
                             </span>
                         </a>
@@ -178,7 +175,7 @@
                             <lord-icon src="https://cdn.lordicon.com/xcrjfuzb.json" colors="primary:#FF731D"
                                 trigger="hover" style="width:30px;height:30px" class="-rotate-90">
                             </lord-icon>
-                            <span class="text-md ml-2 mt-1 hover:text-primary">
+                            <span class="text-md mx-2 mt-1 hover:text-primary">
                                 Credit
                             </span>
                         </a>
@@ -191,10 +188,10 @@
                     <a href="">
                         <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="50" height="50"
                             viewBox="0,0,256,256" class="transition ease-in-out delay-100 hover:scale-125">
-                            <g fill="#ff731d" fill-rule="nonzero" stroke="none" stroke-width="1"
-                                stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10"
-                                stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none"
-                                font-size="none" text-anchor="none" style="mix-blend-mode: normal">
+                                <g fill="#ff731d" fill-rule="nonzero" stroke="none" stroke-width="1"
+                                    stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10"
+                                    stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none"
+                                    font-size="none" text-anchor="none" style="mix-blend-mode: normal">
                                 <g transform="scale(5.12,5.12)">
                                     <path
                                         d="M25,3c-12.15,0 -22,9.85 -22,22c0,11.03 8.125,20.137 18.712,21.728v-15.897h-5.443v-5.783h5.443v-3.848c0,-6.371 3.104,-9.168 8.399,-9.168c2.536,0 3.877,0.188 4.512,0.274v5.048h-3.612c-2.248,0 -3.033,2.131 -3.033,4.533v3.161h6.588l-0.894,5.783h-5.694v15.944c10.738,-1.457 19.022,-10.638 19.022,-21.775c0,-12.15 -9.85,-22 -22,-22z">
@@ -206,10 +203,10 @@
                     <a href="">
                         <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="50" height="50"
                             viewBox="0,0,256,256" class="transition ease-in-out delay-100 hover:scale-125">
-                            <g fill="#ff731d" fill-rule="nonzero" stroke="none" stroke-width="1"
-                                stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10"
-                                stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none"
-                                font-size="none" text-anchor="none" style="mix-blend-mode: normal">
+                                <g fill="#ff731d" fill-rule="nonzero" stroke="none" stroke-width="1"
+                                    stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10"
+                                    stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none"
+                                    font-size="none" text-anchor="none" style="mix-blend-mode: normal">
                                 <g transform="scale(5.12,5.12)">
                                     <path
                                         d="M16,3c-7.16752,0 -13,5.83248 -13,13v18c0,7.16752 5.83248,13 13,13h18c7.16752,0 13,-5.83248 13,-13v-18c0,-7.16752 -5.83248,-13 -13,-13zM16,5h18c6.08648,0 11,4.91352 11,11v18c0,6.08648 -4.91352,11 -11,11h-18c-6.08648,0 -11,-4.91352 -11,-11v-18c0,-6.08648 4.91352,-11 11,-11zM37,11c-1.10457,0 -2,0.89543 -2,2c0,1.10457 0.89543,2 2,2c1.10457,0 2,-0.89543 2,-2c0,-1.10457 -0.89543,-2 -2,-2zM25,14c-6.06329,0 -11,4.93671 -11,11c0,6.06329 4.93671,11 11,11c6.06329,0 11,-4.93671 11,-11c0,-6.06329 -4.93671,-11 -11,-11zM25,16c4.98241,0 9,4.01759 9,9c0,4.98241 -4.01759,9 -9,9c-4.98241,0 -9,-4.01759 -9,-9c0,-4.98241 4.01759,-9 9,-9z">
@@ -221,10 +218,10 @@
                     <a href="">
                         <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="50" height="50"
                             viewBox="0,0,256,256" class="transition ease-in-out delay-100 hover:scale-125">
-                            <g fill="#ff731d" fill-rule="nonzero" stroke="none" stroke-width="1"
-                                stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10"
-                                stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none"
-                                font-size="none" text-anchor="none" style="mix-blend-mode: normal">
+                                <g fill="#ff731d" fill-rule="nonzero" stroke="none" stroke-width="1"
+                                    stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10"
+                                    stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none"
+                                    font-size="none" text-anchor="none" style="mix-blend-mode: normal">
                                 <g transform="scale(10.66667,10.66667)">
                                     <path
                                         d="M2.36719,3l7.0957,10.14063l-6.72266,7.85938h2.64063l5.26367,-6.16992l4.31641,6.16992h6.91016l-7.42187,-10.625l6.29102,-7.375h-2.59961l-4.86914,5.6875l-3.97266,-5.6875zM6.20703,5h2.04883l9.77734,14h-2.03125z">
@@ -240,13 +237,9 @@
 
         </div>
     </div>
-<<<<<<< HEAD
-    <hr class="text-light ml-10 mr-80 mt-1 mb-3">
-    <div class="flex justify-between ml-10 mr-80 text-sm">
-=======
-    <hr class="text-light mx-10 mt-1 mb-3">
-    <div class="flex justify-between mx-10 text-sm">
->>>>>>> 860bdbcd3aa94e659bed4edadb38155e0d2628d6
+
+    <hr class="text-light mx-10 mr-80 mt-1 mb-3">
+    <div class="flex justify-between mx-10 mr-80 text-sm">
         <p>MLG CL's Capstone Project Catering System Web-Based.</p>
         <p>Copyright © 2024. All Right Reserved.</p>
     </div>
