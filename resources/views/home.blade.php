7684c08ab14c354cbd10a629231ce727b8abc5a9
Utrera, DD <126302609+mrdenzeru@users.noreply.github.com>
2024-02-26 12:25:13 +0800

    Users Edit Modal (#38)
    * Tailwind Css & Header
    
    * Tailwind CSS, Header, etc
    
    * Footer Design none Responsive
    
    * Solve the Error in the User-edit Modal
    
    ---------
    
    Co-authored-by: mrdenzeru <ddaveutrer23@gmail.com>

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,13 @@
 @extends('layouts.master')
 @section('content')
  <div class="w-full h-40 bg-light">
+<<<<<<< HEAD
+    <h1 class="text-center text-3xl font-bold underline font-cursive ">
+      TABANG MGA LANGIT
+=======
     <h1 class="text-3xl font-bold underline font-cursive ">
       This is Home Page
+>>>>>>> 860bdbcd3aa94e659bed4edadb38155e0d2628d6
     </h1>
  </div>
 @endsection
--- resources/views/layouts/inc/header.blade.php
+++ resources/views/layouts/inc/header.blade.php
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
+<header class="bg-light">
+=======
 <header>
+>>>>>>> 860bdbcd3aa94e659bed4edadb38155e0d2628d6
     <div class="wave">
         <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
             <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
@@ -15,7 +19,14 @@
             </button>
             {{-- logo --}}
             <a href="{{ url('/home') }}" class="logo-md-sm w-32">
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
 
+>>>>>>> 860bdbcd3aa94e659bed4edadb38155e0d2628d6
+=======
+
+>>>>>>> e52c72dc86f34502c2d93805e998c3ff724f5424
                 <img class="logo-lg w-auto h-auto sm-mobile:w" src="{{ asset('assets/img/logo.png') }}" alt="titans-catering-logo">
             </a>
         </div>
