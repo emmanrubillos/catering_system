10a74e7154f926700819bca2830c728de200c323
mrdenzeru <ddaveutrer23@gmail.com>
2024-03-19 11:40:32 +0800

    updated Make Reservation

---
+++ resources/views/layouts/customer/make_reservation/partials/service/service_selection.blade.php
@@ -0,0 +1,90 @@
+{{--? Event Reservation --}}
+<div class="relative top-[-9rem] hidden">
+
+    <div class="mx-[20rem]">
+        <div class="text-center">
+            <h1 class="relative -bottom-5 text-primary display-font bg-light py-1 px-4 mx-[7.5rem] text-xxl rounded-md">
+                Service Selection
+            </h1>
+            <h2 class="bg-dark px-14 py-7 text-light font-bold rounded-md text-xxl shadow-xl">
+                Note: 
+                <br>
+                <span class="text-xl leading-7 font-semibold">
+                    You Choice Wedding as the Event, so we please choice a service.
+                </span>
+            </h2>
+        </div>
+    </div>
+    {{--! Left Container --}}
+    <div class="flex flex-row gap-5 h-[30rem] mx-20 mt-10">
+        <div class="flex flex-col left-service basis-[45%]">
+            <div class="w-full text-left transition ease-in-out hover:scale-105 z-10">
+                <span class="bg-dark text-light py-4 px-8 uppercase font-bold text-xxl display-font
+                rounded-xl shadow-lg relative -bottom-2">
+                    Wedd Pack
+                </span>
+            </div>
+
+            <div class="bg-light px-8 rounded-xl shadow-xl transition ease-in-out hover:scale-105 z-0">
+                <h4 class="font-bold uppercase display-font text-dark mt-8 mb-3 text-lg 
+                before:border-l-4 before:border-dark before:mr-3">
+                    Wedding Package
+                </h4>
+                <p class="text-dark text-justify font-semibold indent-8">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore, delectus rem. 
+                    Iure consectetur nam laborum delectus, perferendis reprehenderit omnis ea nobis. 
+                    Sequi error dolor doloribus ut sit placeat nemo quia! Lorem ipsum dolor sit amet 
+                    consectetur, adipisicing elit. Dolore, delectus rem. Iure consectetur nam laborum
+                    electus, perferendis reprehenderit omnis ea nobis. Sequi error dolor doloribus ut 
+                    sit placeat nemo quia!
+                </p>
+                <div class="text-center">
+                    <button class="bg-primary text-light py-2 px-12 text-xxl uppercase rounded-lg
+                    font-bold shadow-lg relative -bottom-5 transition ease-in-out hover:bg-primary-dark hover:scale-105">
+                        Select
+                    </button>
+                </div>
+            </div>
+        </div>
+
+        {{--! Middle Container --}}
+        <div class="or basis-[10%] self-center">
+            <h2 class="display-font text-dark drop-shadow-lg text-xxl text-center">
+                OR
+            </h2>
+        </div>
+
+        {{--! Right Container --}}
+        <div class="flex flex-col right-service basis-[45%]">
+            <div class="w-full text-right transition ease-in-out hover:scale-105 z-10">
+                <span class="bg-dark text-light py-4 px-8 uppercase font-bold text-xxl display-font 	
+                rounded-xl shadow-lg relative -bottom-2">
+                    By Pax
+                </span>
+            </div>
+
+            <div class="bg-light px-8 rounded-xl shadow-xl transition ease-in-out hover:scale-105 z-0">
+                <h4 class="font-bold uppercase display-font text-dark mt-8 mb-3 text-lg text-right	
+                after:border-r-4 after:border-dark after:ml-3">
+                    By Per Head
+                </h4>
+                <p class="text-dark text-justify font-semibold indent-8">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore, delectus rem. 
+                    Iure consectetur nam laborum delectus, perferendis reprehenderit omnis ea nobis. 
+                    Sequi error dolor doloribus ut sit placeat nemo quia! Lorem ipsum dolor sit amet 
+                    consectetur, adipisicing elit. Dolore, delectus rem. Iure consectetur nam laborum
+                    electus, perferendis reprehenderit omnis ea nobis. Sequi error dolor doloribus ut 
+                    sit placeat nemo quia!
+                </p>
+                <div class="text-center">
+                    <button class="bg-primary text-light py-2 px-12 text-xxl uppercase rounded-lg
+                    font-bold shadow-lg relative -bottom-5 transition ease-in-out hover:bg-primary-dark hover:scale-105">
+                        Select
+                    </button>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+</div>
