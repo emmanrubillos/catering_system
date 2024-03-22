afe99e4f5443d2b7537638edf51be1bcfad2ab93
mrdenzeru <ddaveutrer23@gmail.com>
2024-03-22 21:51:32 +0800

    Add group-input  in Create and make the Edit

--- resources/views/admin/classification/index.blade.php
+++ resources/views/admin/classification/index.blade.php
@@ -9,8 +9,10 @@
                     <div class="card col-sm-12 mt-5">
                         <div class="card-header breadcrumb-item active d-flex justify-content-between">
                             <div class="d-flex flex-row">
-                                <img width="42" height="42" src="https://img.icons8.com/glyph-neue/64/restaurant-pickup.png" 
-                                alt="restaurant-pickup"/>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="mr-2" style="width: 42px;">
+                                    <!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                                    <path fill="#000000" d="M152.1 38.2c9.9 8.9 10.7 24 1.8 33.9l-72 80c-4.4 4.9-10.6 7.8-17.2 7.9s-12.9-2.4-17.6-7L7 113C-2.3 103.6-2.3 88.4 7 79s24.6-9.4 33.9 0l22.1 22.1 55.1-61.2c8.9-9.9 24-10.7 33.9-1.8zm0 160c9.9 8.9 10.7 24 1.8 33.9l-72 80c-4.4 4.9-10.6 7.8-17.2 7.9s-12.9-2.4-17.6-7L7 273c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l22.1 22.1 55.1-61.2c8.9-9.9 24-10.7 33.9-1.8zM224 96c0-17.7 14.3-32 32-32H480c17.7 0 32 14.3 32 32s-14.3 32-32 32H256c-17.7 0-32-14.3-32-32zm0 160c0-17.7 14.3-32 32-32H480c17.7 0 32 14.3 32 32s-14.3 32-32 32H256c-17.7 0-32-14.3-32-32zM160 416c0-17.7 14.3-32 32-32H480c17.7 0 32 14.3 32 32s-14.3 32-32 32H192c-17.7 0-32-14.3-32-32zM48 368a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"/>
+                                </svg>
                                 <h3 class="mt-2 ml-2 text-dark fw-bold">Classifications</h3>
                             </div>
                             <div class="mt-2">
@@ -58,8 +60,7 @@
                                     </span>
                                 </td>
 
-                                @include('admin.classification.partials.script')
-                                @include('admin.classification.partials._add_classification_modal')
+
                                 @include('admin.classification.partials._edit_classification_modal')
                             </tr>
                             @endforeach
@@ -82,4 +83,5 @@
 </script>
 
 @include('admin.classification.partials._add_classification_modal')
+@include('admin.classification.partials.script')
 @endsection
--- resources/views/admin/classification/partials/_add_classification_modal.blade.php
+++ resources/views/admin/classification/partials/_add_classification_modal.blade.php
@@ -3,28 +3,35 @@
     <div class="modal-dialog modal-md" role="document">
         <div class="modal-content">
             <div class="modal-header bg-success text-white">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="iconmodal">
-                    !--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
-                    <path fill="#ffffff" d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"/>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="iconmodal mr-2">
+                    <!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                    <path fill="#ffffff" d="M152.1 38.2c9.9 8.9 10.7 24 1.8 33.9l-72 80c-4.4 4.9-10.6 7.8-17.2 7.9s-12.9-2.4-17.6-7L7 113C-2.3 103.6-2.3 88.4 7 79s24.6-9.4 33.9 0l22.1 22.1 55.1-61.2c8.9-9.9 24-10.7 33.9-1.8zm0 160c9.9 8.9 10.7 24 1.8 33.9l-72 80c-4.4 4.9-10.6 7.8-17.2 7.9s-12.9-2.4-17.6-7L7 273c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l22.1 22.1 55.1-61.2c8.9-9.9 24-10.7 33.9-1.8zM224 96c0-17.7 14.3-32 32-32H480c17.7 0 32 14.3 32 32s-14.3 32-32 32H256c-17.7 0-32-14.3-32-32zm0 160c0-17.7 14.3-32 32-32H480c17.7 0 32 14.3 32 32s-14.3 32-32 32H256c-17.7 0-32-14.3-32-32zM160 416c0-17.7 14.3-32 32-32H480c17.7 0 32 14.3 32 32s-14.3 32-32 32H192c-17.7 0-32-14.3-32-32zM48 368a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"/>
                 </svg>
                 <h5 class="modal-title fw-bold" id="addClassificationModalLabel">Add Classification</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="iconmodal">
+                        <!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                        <path fill="#ffffff" d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zm79 143c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9z"/>
+                    </svg>
                 </button>
             </div>
             <div class="modal-body">
                 <form action="{{ route('classification.store') }}" method="POST" id="create-classification-form">
                     @csrf
-                    <div class="">
-                        <div class="form-group">
-                            <label class="fw-bold" for="name">Name</label>
-                            <input type="text" class="form-control" id="name" name="name">
+                    <div class="row">
+                        <div class="col-6 form-group">
+                            <label class="fw-bold bg-success text-white px-4 py-1 rounded" for="name">Name</label>
+                            <input type="text" class="form-control" id="add-classification-name" name="name" placeholder="Class Name">
+                        </div>
+                        <div class="col-6 form-group">
+                            <label class="fw-bold bg-success text-white px-4 py-1 rounded" for="group">Group</label>
+                            <input type="text" class="form-control" id="add-classification-group" name="group" placeholder="Class Group">
                         </div>
                     </div>
 
-                    <div class=" align-items-center">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                        <button type="submit" class="btn btn-success" id="save-classification-btn">Save</button>
+                    <div class="modal-footer">
+                        <button type="button" class="fw-bold btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="submit" class="fw-bold btn btn-success" id="save-classification-btn">Save</button>
                     </div>
                 </form>
             </div>
