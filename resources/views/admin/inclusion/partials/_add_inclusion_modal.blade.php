260c5032a9c9f5a7da1639c73a5802bea2652af0
mrdenzeru <ddaveutrer23@gmail.com>
2024-03-23 00:00:26 +0800

    Search Generator, Selected Type, Create of Inclusion

--- resources/views/admin/inclusion/partials/_add_inclusion_modal.blade.php
+++ resources/views/admin/inclusion/partials/_add_inclusion_modal.blade.php
@@ -2,13 +2,17 @@
 <div class="modal fade" id="addInclusionModal" tabindex="-1" role="dialog" aria-labelledby="addUserModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
-            <div class="modal-header bg-primary text-white">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="iconmodal">
-                    !--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#ff371d" d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"/>
+            <div class="modal-header bg-success text-white">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" class="iconmodal mr-3" style="width: 42px;">
+                    <!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                    <path fill="#ffffff" d="M308.5 135.3c7.1-6.3 9.9-16.2 6.2-25c-2.3-5.3-4.8-10.5-7.6-15.5L304 89.4c-3-5-6.3-9.9-9.8-14.6c-5.7-7.6-15.7-10.1-24.7-7.1l-28.2 9.3c-10.7-8.8-23-16-36.2-20.9L199 27.1c-1.9-9.3-9.1-16.7-18.5-17.8C173.9 8.4 167.2 8 160.4 8h-.7c-6.8 0-13.5 .4-20.1 1.2c-9.4 1.1-16.6 8.6-18.5 17.8L115 56.1c-13.3 5-25.5 12.1-36.2 20.9L50.5 67.8c-9-3-19-.5-24.7 7.1c-3.5 4.7-6.8 9.6-9.9 14.6l-3 5.3c-2.8 5-5.3 10.2-7.6 15.6c-3.7 8.7-.9 18.6 6.2 25l22.2 19.8C32.6 161.9 32 168.9 32 176s.6 14.1 1.7 20.9L11.5 216.7c-7.1 6.3-9.9 16.2-6.2 25c2.3 5.3 4.8 10.5 7.6 15.6l3 5.2c3 5.1 6.3 9.9 9.9 14.6c5.7 7.6 15.7 10.1 24.7 7.1l28.2-9.3c10.7 8.8 23 16 36.2 20.9l6.1 29.1c1.9 9.3 9.1 16.7 18.5 17.8c6.7 .8 13.5 1.2 20.4 1.2s13.7-.4 20.4-1.2c9.4-1.1 16.6-8.6 18.5-17.8l6.1-29.1c13.3-5 25.5-12.1 36.2-20.9l28.2 9.3c9 3 19 .5 24.7-7.1c3.5-4.7 6.8-9.5 9.8-14.6l3.1-5.4c2.8-5 5.3-10.2 7.6-15.5c3.7-8.7 .9-18.6-6.2-25l-22.2-19.8c1.1-6.8 1.7-13.8 1.7-20.9s-.6-14.1-1.7-20.9l22.2-19.8zM112 176a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zM504.7 500.5c6.3 7.1 16.2 9.9 25 6.2c5.3-2.3 10.5-4.8 15.5-7.6l5.4-3.1c5-3 9.9-6.3 14.6-9.8c7.6-5.7 10.1-15.7 7.1-24.7l-9.3-28.2c8.8-10.7 16-23 20.9-36.2l29.1-6.1c9.3-1.9 16.7-9.1 17.8-18.5c.8-6.7 1.2-13.5 1.2-20.4s-.4-13.7-1.2-20.4c-1.1-9.4-8.6-16.6-17.8-18.5L583.9 307c-5-13.3-12.1-25.5-20.9-36.2l9.3-28.2c3-9 .5-19-7.1-24.7c-4.7-3.5-9.6-6.8-14.6-9.9l-5.3-3c-5-2.8-10.2-5.3-15.6-7.6c-8.7-3.7-18.6-.9-25 6.2l-19.8 22.2c-6.8-1.1-13.8-1.7-20.9-1.7s-14.1 .6-20.9 1.7l-19.8-22.2c-6.3-7.1-16.2-9.9-25-6.2c-5.3 2.3-10.5 4.8-15.6 7.6l-5.2 3c-5.1 3-9.9 6.3-14.6 9.9c-7.6 5.7-10.1 15.7-7.1 24.7l9.3 28.2c-8.8 10.7-16 23-20.9 36.2L315.1 313c-9.3 1.9-16.7 9.1-17.8 18.5c-.8 6.7-1.2 13.5-1.2 20.4s.4 13.7 1.2 20.4c1.1 9.4 8.6 16.6 17.8 18.5l29.1 6.1c5 13.3 12.1 25.5 20.9 36.2l-9.3 28.2c-3 9-.5 19 7.1 24.7c4.7 3.5 9.5 6.8 14.6 9.8l5.4 3.1c5 2.8 10.2 5.3 15.5 7.6c8.7 3.7 18.6 .9 25-6.2l19.8-22.2c6.8 1.1 13.8 1.7 20.9 1.7s14.1-.6 20.9-1.7l19.8 22.2zM464 304a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"/>
                 </svg>
-                <h5 class="modal-title" id="addUserModalLabel">Add Inclusion</h5>
+                <h5 class="modal-title fw-bold" id="addUserModalLabel">Add Inclusion</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="iconmodal">
+                        <!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                        <path fill="#ffffff" d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zm79 143c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9z"/>
+                    </svg>
                 </button>
             </div>
             <div class="modal-body">
@@ -17,38 +21,96 @@
                     <div class="row">
                         <div class="col-md-6">
                             <div class="form-group">
-                                <label for="name">Name</label>
-                                <input type="text" class="form-control" id="name" name="name">
+                                <label class="fw-bold bg-success text-white px-4 py-1 rounded" for="name">Name</label>
+                                <input type="text" class="form-control" id="add-inclusion-name" name="name" placeholder="Incluson Name">
+                            </div>
+                            <div class="form-group">
+                                <span class="bg-success pl-3 pr-4 py-2 rounded">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" style="width: 20px;" class="mr-2">
+                                        <!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                                        <path fill="#ffffff" d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/>
+                                    </svg>
+                                    <label class="fw-bold text-white" for="name">Classification Type</label> 
+                                </span>                               
+
+                                <input type="text" class="form-control mt-1" id="search-classification-type" name="search" placeholder="Classification Type">
+                            </div>
+                            <div class="col-md-12 mb-4 overflow-auto add-scroll" style="height:200px;">
+                                <div class="form-group">
+                                    <label class="fw-bold bg-success text-white px-4 py-1 rounded" for="classification_id">Types</label>
+                                    <div>
+                                        @foreach ($classifications as $classification)
+                                            <div class="form-check">
+                                                <input class="form-check-input" type="checkbox" id="add-classification-check" name="classification_id[]" value="{{ $classification->id }}">
+                                                <label class="form-check-label fw-bold" for="classification_id{{ $classification->id }}">
+                                                    {{ $classification->name }}
+                                                </label>
+                                            </div>
+                                        @endforeach
+                                    </div>
+                                </div>
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
-                                <label for="classification_id">Type</label>
-                                <div>
-                                    @foreach ($classifications as $classification)
-                                        <div class="form-check">
-                                            <input class="form-check-input" type="checkbox" name="classification_id[]" value="{{ $classification->id }}">
-                                            <label class="form-check-label" for="classification_id{{ $classification->id }}">
-                                                {{ $classification->name }}
-                                            </label>
-                                        </div>
-                                    @endforeach
+                                <label class="fw-bold bg-success text-white px-4 py-1 rounded" for="description">Description</label>
+                                <textarea type="text" class="form-control" placeholder="Enter a description ...." name="description" id="add-inclusion-description" rows="5"></textarea>
+                            </div>
+                            <div class="col-md-12 mb-4 overflow-auto add-scroll" style="height:200px;">
+                                <div class="form-group" style="height:150px;">
+                                    <label class="fw-bold bg-success text-white px-4 py-1 rounded" for="classification_id_selected">Selected Types</label>
+                                    <ul id="selected-types-list" class="list-unstyled">
+                                        
+                                    </ul>
                                 </div>
-                        </div>
-                        <div class="col-md-12">
-                            <div class="form-group">
-                                <label for="description">Description</label>
-                                <input type="text" class="form-control" id="description" name="description">
                             </div>
                         </div>
                     </div>
+
                     <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                        <button type="submit" class="btn btn-primary" id="save-inclusion-btn">Save</button>
+                        <button type="button" class="btn btn-secondary fw-bold" data-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-success fw-bold" id="save-inclusion-btn">Save</button>
                     </div>
+
+                    
                 </form>
             </div>
         </div>
     </div>
 </div>
 
+
+<script>
+    // Search Generator for Classification Type
+    $(document).ready(function() {
+        $('#search-classification-type').on('input', function() {
+            var searchText = $(this).val().toLowerCase();
+            $('.form-check-label').each(function() {
+                var classificationName = $(this).text().toLowerCase();
+                if (classificationName.includes(searchText)) {
+                    $(this).parent().show();
+                } else {
+                    $(this).parent().hide();
+                }
+            });
+        });
+    });
+
+    $(document).ready(function() {
+        $('input[name="classification_id[]"]').on('change', function() {
+            var selectedTypes = [];
+            $('input[name="classification_id[]"]:checked').each(function() {
+                selectedTypes.push($(this).siblings('label').text());
+            });
+
+            var listItems = selectedTypes.map(function(type) {
+                return '<li class="badge badge-secondary mb-1">' + type + '</li> <br>';
+            });
+
+            $('#selected-types-list').html(listItems.join(''));
+        });
+    });
+
+
+    
+</script>
