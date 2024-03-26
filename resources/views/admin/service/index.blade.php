f6e8fa1ba1e85407b3198def936f0bfc416d4d24
emmanrubillos <126300648+emmanrubillos@users.noreply.github.com>
2024-03-26 08:45:39 +0800

    Still working on the Edit Service Functionality

--- resources/views/admin/service/index.blade.php
+++ resources/views/admin/service/index.blade.php
@@ -74,7 +74,11 @@
     </div>
 </div>
 
-
+<script>
+    $(".service-checkbox").on('click', function(event) {
+       let isChecked = $(this).prop('checked');
+    })
+</script>
 {{-- <script src="https://cdn.datatables.net/2.0.2/js/dataTables.bootstrap5.js"></script> --}}
 <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
